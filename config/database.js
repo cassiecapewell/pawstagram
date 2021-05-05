7e88d417a4178f9d56e11644eb4fd3e2c766f6c1
Jamison McCarty <jmcc13@vt.edu>
2021-05-05 00:37:46 -0400

    should fix issue

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
 
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
+    const conn = await mongoose.connect(DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
